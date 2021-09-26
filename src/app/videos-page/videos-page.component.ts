280c605b7eb703219494cea91742fec9a80eb62e
austin.moore <austin.moore@resonantsciences.com>
2021-09-26 15:39:37 -0400

    Add functions for connecting to the google api client

--- src/app/videos-page/videos-page.component.ts
+++ src/app/videos-page/videos-page.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
+declare var gapi: any;
+
+function authenticate() {
+  return gapi.auth2.getAuthInstance()
+}
+function loadClient() {
+  gapi.client.setApiKey(environment.GAPI_KEY);
+  return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+  .then(function() { console.log("GAPI client loaded for API"); },
+    function(err : Error) { console.error("Error loading GAPI client for API", err); });
+}
 @Component({
   selector: 'app-videos-page',
   templateUrl: './videos-page.component.html',
