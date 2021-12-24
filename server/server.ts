56e9fa0ea1141ce3eac4686657be79ba9b81a18c
Israel Bravo <bravoil@mail.uc.edu>
2021-12-24 13:40:45 -0500

    serve up angular application

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,12 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import helmet from "helmet";
 import cors from "cors";
+import * as path from "path";
 
 const app = express();
+
+// Path to our built Angular application
+const APP_LOCATION = path.resolve(__dirname + "/../app/dist/KnownShysters/");
 
 // Use Helmet to protect against common web vulnerabilities
 app.use(helmet());
@@ -11,6 +15,15 @@ app.use(helmet());
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 app.use(cors());
 
+app.use(express.static(APP_LOCATION));
+
 app.listen(4201, "127.0.0.1", () => {
     console.log("Server is now listening on 4201")
+})
+
+// TODO: add API routing here
+
+// Respond to all other requests with our Angular front-end
+app.get('*', (req: Request, res: Response) => {
+    res.sendFile(APP_LOCATION+"index.html");
 })
